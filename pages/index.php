790184cad2305c0881f354a813c0e9842ee9c01a
Fedefemia <10934171@itisrossi.vi.it>
2025-12-14 15:39:47 +0100

    err on log

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,9 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_errors.log');
 
 session_start();
 require_once 'db_config.php';
